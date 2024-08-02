6961ad4493779d8a1add4c43b5257f143010a2fd
Torres <20233tn105@utez.edu.mx>
2024-08-02 17:51:56 -0600

    Version para servidor

--- src/main/java/mx/edu/utez/saac/utils/DatabaseConnectionManager.java
+++ src/main/java/mx/edu/utez/saac/utils/DatabaseConnectionManager.java
@@ -6,7 +6,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class DatabaseConnectionManager {
-    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/integradoradb";
+    private static final String JDBC_URL = "jdbc:mysql://52.207.238.82:3306/integradoradb";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "Password123?";
     private static final HikariConfig config = new HikariConfig();
