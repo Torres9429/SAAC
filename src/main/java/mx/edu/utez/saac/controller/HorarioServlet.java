76449769c9bf721bbf6633246997518c220f4ae6
Torres <20233tn105@utez.edu.mx>
2024-08-20 16:44:28 -0600

    Actualice conexion

--- src/main/java/mx/edu/utez/saac/controller/HorarioServlet.java
+++ src/main/java/mx/edu/utez/saac/controller/HorarioServlet.java
@@ -26,26 +26,39 @@ public class HorarioServlet extends HttpServlet {
 
         String horaInicioStr = req.getParameter("hora-inicio") + ":00";
         String horaFinStr = req.getParameter("hora-fin") + ":00";
+        LocalDateTime fechaActual = LocalDateTime.now();
 
-        horario.setHora_inicio(Time.valueOf(horaInicioStr));
-        horario.setHora_fin(Time.valueOf(horaFinStr));
-        horario.setDia(Date.valueOf(req.getParameter("dia")));
-        horario.setMateria(Integer.parseInt(req.getParameter("materia")));
-        horario.setId_usuario(Integer.parseInt(req.getParameter("id_usuario")));
-        horario.setId_aula(Integer.parseInt(req.getParameter("aula")));
+        // Convertir la fecha recibida a LocalDateTime
+        Date fechaDia = Date.valueOf(req.getParameter("dia"));
+        LocalDateTime fechaRecibida = fechaDia.toLocalDate().atTime(Time.valueOf(horaInicioStr).toLocalTime());
 
-        HorarioDao dao = new HorarioDao();
-        if (dao.guardarHorario(horario)){
-            mensaje = "Horario agregado con éxito";
-            System.out.println("exito");
-        }else {
-            mensaje = "Ha ocurrido un error, por favor, inténtelo más tarde";
-            System.out.println("error");
+        // Validar si la fecha recibida es anterior a la fecha actual
+        if (fechaRecibida.isBefore(fechaActual)) {
+            mensaje = "No se puede agregar un horario con una fecha anterior a la actual.";
+            System.out.println("Error: Fecha anterior a la actual");
+        } else {
+            horario.setHora_inicio(Time.valueOf(horaInicioStr));
+            horario.setHora_fin(Time.valueOf(horaFinStr));
+            horario.setDia(fechaDia);
+            horario.setMateria(Integer.parseInt(req.getParameter("materia")));
+            horario.setId_usuario(Integer.parseInt(req.getParameter("id_usuario")));
+            horario.setId_aula(Integer.parseInt(req.getParameter("aula")));
+
+            HorarioDao dao = new HorarioDao();
+            if (dao.guardarHorario(horario)){
+                mensaje = "Horario agregado con éxito";
+                System.out.println("exito");
+            } else {
+                mensaje = "Ha ocurrido un error, por favor, inténtelo más tarde";
+                System.out.println("error");
+            }
         }
+
         HttpSession session = req.getSession();
         session.setAttribute("mensaje", mensaje);
 
         resp.sendRedirect("docente/agregarAsesoria.jsp");
     }
+
 
 }
